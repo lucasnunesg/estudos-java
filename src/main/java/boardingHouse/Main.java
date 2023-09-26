3785e5b4070ad607bbeb24cb94eb854108b69a03
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-09-26 16:03:39 -0300

    added `boardingHouse` exercise

---
+++ src/main/java/boardingHouse/Main.java
@@ -0,0 +1,35 @@
+package boardingHouse;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+        Room[] rooms = new Room[10];
+
+        for (int i=0; i<n; i++) {
+            System.out.printf("%nRoom #%d%n", i+1);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int roomNumber = sc.nextInt();
+            rooms[roomNumber] = new Room(name, email, roomNumber);
+        }
+
+        System.out.printf("%nBusy rooms:%n");
+        for (int i=0; i<10; i++) {
+            if(rooms[i] != null) {
+                System.out.print(i + ": " + rooms[i]);
+            }
+        }
+        sc.close();
+    }
+
+}
---
+++ src/main/java/boardingHouse/Room.java
@@ -0,0 +1,30 @@
+package boardingHouse;
+
+public class Room {
+    private String name;
+    private String email;
+    private int roomNumber;
+
+    public Room(String name, String email, int roomNumber) {
+        this.name = name;
+        this.email = email;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s%n", name, email);
+    }
+}
