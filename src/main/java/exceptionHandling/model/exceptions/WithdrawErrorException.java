43af644b37ef97dadf796ce19cb80ba329b5cdf9
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2023-10-16 20:03:01 -0300

    added `serialVersionUID`

--- src/main/java/exceptionHandling/model/exceptions/WithdrawErrorException.java
+++ src/main/java/exceptionHandling/model/exceptions/WithdrawErrorException.java
@@ -1,6 +1,7 @@
 package exceptionHandling.model.exceptions;
 
 public class WithdrawErrorException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
 
     public WithdrawErrorException(String msg) {
         super(msg);
